61be31120a7f7c719bc13a9f1f1b9ed97b398196
Ivetta Kosova <ivetta.kosova@opensoftdev.ru>
2013-03-21 17:37:03 +0400

    mark-up2

--- sites/all/themes/generation_plus/page.tpl.php
+++ sites/all/themes/generation_plus/page.tpl.php
@@ -18,7 +18,7 @@
 // print $photoreportsBlock;
 ?>
 
-<div id="wrapper">
+<div id="wrapper" bgcolor="chucknorris">
     <div id="wrapper-main">
         <div id="header">
             <div id="top-menu"></div>
@@ -29,17 +29,31 @@
             <?php print render($page['content']['superfish_1']);?>
         </div>
         <div id="wrapper-content">
-            <div id="content"></div>
+            <div id="content">
+                <div id="video"></div>
+                <div id="subscription"></div>
+            </div>
             <div id="block-audios"></div>
         </div>
 
-        <div id="wrapper-reports">
-            <div id="header-reports"></div>
-            <div id="block-reports"></div>
+        <div id="wrapper-photoreports">
+            <div id="header-photoreports">
+                <img id="logo-small" src="sites/all/themes/generation_plus/images/logo_small.png">
+                <h3>ФОТООТЧЕТЫ</h3>
+            </div>
+            <div id="block-photoreports"></div>
         </div>
 
-        <div id="block-news"></div>
-        <div id="footer"></div>
+        <div id="block-articles"></div>
+        <div id="wrapper-footer">
+            <div id="qr-code"></div>
+            <div id="footer" bgcolor="chucknorris">
+                <div id="footer-text">
+                    <div id="social"></div>
+                    <div>Copyright © 2003-2013 Поколение Plus</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
