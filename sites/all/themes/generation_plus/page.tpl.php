8cca0fb2ddbbd51eec1c0ce5a20994f09ff0bfc5
vladimir.prudilin <vladimir.prudilin@opensoftdev.ru>
2013-03-27 14:28:51 +0400

    Simplenews form added

--- sites/all/themes/generation_plus/page.tpl.php
+++ sites/all/themes/generation_plus/page.tpl.php
@@ -16,48 +16,48 @@
                 ?>
             </div>
         </div>
-        <div class="shadow">
-            <div id="wrapper-main-menu">
-                <div id="main-menu">
-                     <?php
-                        $block = module_invoke('menu_block', 'block_view', 2);
-                        print render($block['content']['#content']);
-                    ?>
-                </div>
-                <div id="main-menu-sub">
-                    <?php
-                        $block = module_invoke('menu_block', 'block_view', 1);
-                        print render($block['content']['#content']);
-                    ?>
-                </div>
+        <div id="wrapper-main-menu">
+            <div id="main-menu">
+                 <?php
+                    $block = module_invoke('menu_block', 'block_view', 2);
+                    print render($block['content']['#content']);
+                ?>
             </div>
-            <div id="wrapper-content">
-                <div id="content">
-                    <div id="video">
-                        <video width="576" height="356" controls>
-                            <source src="sites/all/themes/generation_plus/images/small.ogg" type="video/ogg">
-                            <source src="sites/all/themes/generation_plus/images/small.webm" type="video/webm"/>
-                            <source src="sites/all/themes/generation_plus/images/small.mp4" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                    <div id="subscription">
-                        <?php
-                            print drupal_render(drupal_get_form('opensoft_mailing_form'));
-                        ?>
-                    </div>
-
+            <div id="main-menu-sub">
+                <?php
+                    $block = module_invoke('menu_block', 'block_view', 1);
+                    print render($block['content']['#content']);
+                ?>
+            </div>
+        </div>
+        <div id="wrapper-content">
+            <div id="content">
+                <div id="video">
+                    <video width="576" height="356" controls>
+                        <source src="sites/all/themes/generation_plus/images/small.ogg" type="video/ogg">
+                        <source src="sites/all/themes/generation_plus/images/small.webm" type="video/webm"/>
+                        <source src="sites/all/themes/generation_plus/images/small.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
-                <div id="block-audios">
+                <div id="subscription">
                     <?php
-                        $block = module_invoke('views', 'block_view', 'recent_audio-block');
-                        print render($block);
+                        $block = module_invoke('simplenews', 'block_view', '1');
+                        print render($block['content']);
                     ?>
                 </div>
+
+            </div>
+            <div class='clear'></div>
+            <div id="block-audios">
+                <?php
+                    $block = module_invoke('views', 'block_view', 'recent_audio-block');
+                    print render($block);
+                ?>
             </div>
         </div>
 
-        <div id="wrapper-photoreports" class="shadow">
+        <div id="wrapper-photoreports">
             <div id="header-photoreports">
                 <img id="logo-small" src="sites/all/themes/generation_plus/images/logo-small.png">
                 <h3>ФОТООТЧЕТЫ</h3>
@@ -70,13 +70,13 @@
             </div>
         </div>
 
-        <div id="block-articles" class="shadow">
+        <div id="block-articles">
             <?php
                 $block = module_invoke('views', 'block_view', 'recent_articles-block');
                 print render($block);
             ?>
         </div>
-        <div id="wrapper-footer" class="shadow">
+        <div id="wrapper-footer">
             <div id="qr-code"></div>
             <div id="footer">
                     Copyright © 2003-2013 Поколение Plus
@@ -87,6 +87,7 @@
 
 </div>
 
+<div class='clear'</div>
 <?php if ($page['sidebar_first']): ?>
     <div id="sidebar-first" class="column sidebar"><div class="section">
             <?php print render($page['sidebar_first']); ?>
--- sites/all/themes/generation_plus/simplenews-block.tpl.php
+++ sites/all/themes/generation_plus/simplenews-block.tpl.php
@@ -2,4 +2,6 @@
     Подпишись на нашу рассылку и получи<br />
     <span>бесплатный видео курс!</span>
 </p>
-<?php print drupal_render_children($form); ?>
+
+<?php
+print render($form);
