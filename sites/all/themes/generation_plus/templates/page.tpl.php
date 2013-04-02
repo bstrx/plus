2d1bc263c5392dc5a6ca021499e029ca46e01aba
vladimir.prudilin <vladimir.prudilin@opensoftdev.ru>
2013-04-02 13:07:50 +0400

    Created page--front.tpl

--- sites/all/themes/generation_plus/templates/page.tpl.php
+++ sites/all/themes/generation_plus/templates/page.tpl.php
@@ -41,7 +41,6 @@
         </div>
 
         <div class="shadow">
-
             <div id="wrapper-main-menu">
                 <div id="main-menu">
                     <?php
@@ -59,58 +58,11 @@
                     ?>
                 </div>
             </div>
-
-            <div id="wrapper-content">
-                <div id="content" class="rounded">
-                    <div id="video" class="rounded">
-                        <object width="576" height="356">
-                            <param name="movie" value="http://www.youtube.com/v/c-mzGDaeoSQ?version=3&amp;hl=ru_RU&amp;rel=0"></param>
-                            <param name="allowFullScreen" value="true"></param>
-                            <param name="allowscriptaccess" value="always"></param>
-                            <embed src="http://www.youtube.com/v/c-mzGDaeoSQ?version=3&amp;hl=ru_RU&amp;rel=0" type="application/x-shockwave-flash" width="576" height="356" allowscriptaccess="always" allowfullscreen="true"></embed>
-                        </object>
-                    </div>
-                    <div id="subscription">
-                        <?php
-                            $block = module_invoke('simplenews', 'block_view', '1');
-                            print render($block['content']);
-                        ?>
-                        <div id="share">
-                            <img src="<?php print $directory ?>/images/share.png" width="21" height="23">
-                            <div id="share-icons">
-                                <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="none" data-yashareQuickServices="vkontakte,twitter,odnoklassniki"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div id="block-audios">
-                    <?php
-                        $block = module_invoke('views', 'block_view', 'recent_audio-block');
-                        print render($block);
-                    ?>
-                </div>
-            </div>
-
         </div>
 
-        <div id="wrapper-photoreports" class="shadow">
-            <div id="header-photoreports">
-                <img id="logo-small" src="<?php print $directory ?>/images/logo-small.png">
-                <h3>ФОТООТЧЕТЫ</h3>
-            </div>
-            <div id="block-photoreports">
-                <?php
-                    $block = module_invoke('views', 'block_view', 'recent_photoreports-block');
-                    print render($block);
-                ?>
-            </div>
-        </div>
-
-        <div id="block-articles" class="shadow">
+        <div class="shadow" id="wrapper-content">
             <?php
-                $block = module_invoke('views', 'block_view', 'recent_articles-block');
-                print render($block);
+                print render($page['content']);
             ?>
         </div>
 
