3891bfe3c125cbff802bab0b8a66c954acdc07d9
Ivetta Kosova <ivetta.kosova@opensoftdev.ru>
2013-04-03 17:10:24 +0400

    photogallery

--- sites/all/themes/generation_plus/templates/page.tpl.php
+++ sites/all/themes/generation_plus/templates/page.tpl.php
@@ -62,11 +62,6 @@
         </div>
 
         <div id="wrapper-content" class="shadow">
-            <?php if ($title): ?>
-                <h1 class="title" id="page-title">
-                    <?php print $title; ?>
-                </h1>
-            <?php endif; ?>
             <?php print render($page['content']); ?>
         </div>
 
