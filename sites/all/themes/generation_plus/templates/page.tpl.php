294bdf67b7df0e6fcaa70d3d9af31b9e6b028713
Ivetta Kosova <ivetta.kosova@opensoftdev.ru>
2013-04-09 12:23:58 +0400

    titles fixed

--- sites/all/themes/generation_plus/templates/page.tpl.php
+++ sites/all/themes/generation_plus/templates/page.tpl.php
@@ -5,6 +5,9 @@
         <?php print render($page['header']); ?>
 
         <div id="wrapper-content" class="shadow">
+            <h1 class="title" id="page-title">
+                <?php print render($title); ?>
+            </h1>
             <?php print render($page['content']); ?>
         </div>
 
