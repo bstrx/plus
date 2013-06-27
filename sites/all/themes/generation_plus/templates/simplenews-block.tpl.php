1036331405fd385ada82c403e77e8ea45baa8a06
vladimir.prudilin <v.prudilin@simbirsoft.com>
2013-06-27 18:40:41 +0400

    Simplenews block create cookie on subscription

--- sites/all/themes/generation_plus/templates/simplenews-block.tpl.php
+++ sites/all/themes/generation_plus/templates/simplenews-block.tpl.php
@@ -1,6 +1,17 @@
-<p>
-    Подпишись на нашу рассылку и получи<br />
-    <span>бесплатный видео курс!</span>
-</p>
+<?php if (!checkSimplenewsSubscription()): ?>
 
-<?php print render($form); ?>
+    <p>
+        Подпишись на нашу рассылку и получи<br />
+        <span>бесплатный видео курс!</span>
+    </p>
+
+    <?php print render($form); ?>
+
+<?php else: ?>
+
+    <p>
+        Вы подписаны на нашу рассылку!
+    </p>
+
+<?php endif ?>
+
